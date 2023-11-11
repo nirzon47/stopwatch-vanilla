b720f53bfdb29975c054909007900175bd793c51
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-11 16:32:01 +0530

    UI v1

---
+++ script.js
@@ -0,0 +1,12 @@
+// DOM Elements
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+const milliseconds = document.getElementById('milliseconds')
+
+const start = document.getElementById('start')
+
+// Event Listeners
+start.addEventListener('click', () => {
+	console.log('start')
+})
