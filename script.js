9c7afedef5c756d7e7176e3989ce640c45f9f50e
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-11 19:05:17 +0530

    optimized stopwatch logic

--- script.js
+++ script.js
@@ -10,68 +10,51 @@ let time
 
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
+const reset = document.getElementById('reset')
 
 // Event Listeners
-start.addEventListener('click', () => {
+start.addEventListener('click', (event) => {
+	event.stopPropagation()
+
 	startStopwatch()
+})
+
+stop.addEventListener('click', (event) => {
+	event.stopPropagation()
+
+	stopStopwatch()
 })
 
 // Functions
 const startStopwatch = () => {
-	if (!running) {
-		startTime = new Date().getTime()
-		time = setInterval(() => {
-			updateMilliseconds()
-		}, 10)
-
-		running = true
-	} else {
+	if (running) {
 		return
 	}
+
+	startTime = startTime || new Date().getTime()
+	time = setInterval(() => {
+		updateTime()
+	}, 10)
+
+	running = true
 }
 
-const updateMilliseconds = () => {
-	const currentTime = new Date().getTime()
-	const elapsedTime = currentTime - startTime
-	const updatedMilliseconds = elapsedTime % 1000
+const stopStopwatch = () => {
+	if (running) {
+		running = false
+		clearInterval(time)
+	}
+}
+
+const updateTime = () => {
+	const elapsed = Date.now() - startTime
+	const updatedHours = Math.floor(elapsed / 3600000)
+	const updatedMinutes = Math.floor((elapsed % 3600000) / 60000)
+	const updatedSeconds = Math.floor((elapsed % 60000) / 1000)
+	const updatedMilliseconds = elapsed % 1000
 
 	milliseconds.textContent = updatedMilliseconds.toString().padStart(3, '000')
-
-	if (elapsedTime >= 1000) {
-		updateSeconds()
-		startTime = currentTime
-	}
-}
-
-const updateSeconds = () => {
-	const secondsValue = parseInt(seconds.style.getPropertyValue('--value'))
-
-	if (secondsValue < 59) {
-		seconds.style.setProperty('--value', secondsValue + 1)
-	} else {
-		updateMinutes()
-		seconds.style.setProperty('--value', 0)
-	}
-}
-
-const updateMinutes = () => {
-	const minutesValue = parseInt(minutes.style.getPropertyValue('--value'))
-
-	if (minutesValue < 59) {
-		minutes.style.setProperty('--value', minutesValue + 1)
-	} else {
-		updateHours()
-		minutes.style.setProperty('--value', 0)
-	}
-}
-
-const updateHours = () => {
-	const hoursValue = parseInt(hours.style.getPropertyValue('--value'))
-
-	if (hoursValue < 59) {
-		hours.style.setProperty('--value', hoursValue + 1)
-	} else {
-		clearInterval(time)
-		alert('Max time reached!')
-	}
+	seconds.style.setProperty('--value', updatedSeconds)
+	minutes.style.setProperty('--value', updatedMinutes)
+	hours.style.setProperty('--value', updatedHours)
 }
